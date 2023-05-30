67e1a6eef12fbafd12c4a7dabc14d4b9d8720695
avya1 <91697572+avya1@users.noreply.github.com>
2023-05-30 14:49:44 +0100

    'thisIsFirstCommitOnTrailRepo'

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress"/>
+describe('this is my trial test suit',()=>{
+    it('visit shein page and verify it',()=>{
+        cy.visit('https://www.shein.co.uk/')
+        cy.get('button').contains('Accept').click()
+        cy.get('div[class="c-coupon-box"] i[class="iconfont icon-close she-close"]').click()
+        cy.get('#header_logo_icon').should('be.visible')
+    })
+    })
+    
