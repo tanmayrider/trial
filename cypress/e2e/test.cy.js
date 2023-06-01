9e54ccdf71daadd23924654b6fc74080d32bf55a
avya1 <91697572+avya1@users.noreply.github.com>
2023-06-01 23:00:10 +0100

    added on cy.log

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -5,6 +5,7 @@ describe('this is my trial test suit',()=>{
         cy.get('button').contains('Accept').click()
         cy.get('div[class="c-coupon-box"] i[class="iconfont icon-close she-close"]').click()
         cy.get('#header_logo_icon').should('be.visible')
+        cy.log('checking auto trigger')
     })
     })
     
